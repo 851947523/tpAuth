b980ec951d506d23b0ddef1fff3544ee3b6a33d2
gaozhe <851947523@qq.com>
2024-03-30 16:18:51 +0800

    更新

--- src/core/Core.php
+++ src/core/Core.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace Gz\TpAuth\core;
+
+use Composer\Script\Event;
+
 /**
  *  editor: gz,
  *  motto: 大自然的搬运工
@@ -11,13 +15,21 @@ class Core
      * 处理文件
      * @return void
      */
-    public static function handle(){
-        $rootPath = app()->getRootPath();
-        $toFile = $rootPath.'/config/auth.php';
+    public static function handle()
+    {
+       self::createConfig();
+    }
+
+    /**
+     * 创建配置文件
+     * @return void
+     */
+    private  static  function createConfig(){
+        $toFile = './config/auth.php';
         $fromFile = __DIR__.'/../config/auth.php';
-        if (!file_exists($fromFile)) {
+        if (!file_exists($toFile)) {
             copy($fromFile,$toFile);
         }
-     }
+    }
 
 }
